bc298b45324d9d8afc24d332e78917c4c9f02bc0
Ewelina <ewelina@ekopacz.pl>
2022-03-17 21:43:02 +0100

    added validation

--- src/components/CalendarForm.js
+++ src/components/CalendarForm.js
@@ -13,26 +13,92 @@ export default class CalendarForm extends React.Component {
     renderForm(){
         return (
             <form className='form__container' onSubmit={this.submitHandler}>
-                <label className='form__label'>Imię: <input name='fristName' value={this.state.firstName} className='form__input' onChange={this.inputHandler} /></label>
+                <label className='form__label'>Imię: <input name='firstName' value={this.state.firstName} className='form__input' onChange={this.inputHandler} /></label>
 
                 <label className='form__label'>Nazwisko: <input name='lastName' value={this.state.lasttName} className='form__input' onChange={this.inputHandler} /></label>
 
                 <label className='form__label'>Email: <input name='email' value={this.state.email} className='form__input' onChange={this.inputHandler} /></label>
 
-                <label className='form__label'>Data: <input name='date' value={this.state.date} className='form__input' onChange={this.inputHandler} /></label>
+                <label className='form__label'>Data: <input name='date' value={this.state.date} className='form__input' onChange={this.inputHandler} placeholder={'YYYY-MM-DD'}/></label>
 
-                <label className='form__label'>Godzina: <input name='hour' value={this.state.hour} className='form__input' onChange={this.inputHandler} /></label>
+                <label className='form__label'>Godzina: <input name='hour' value={this.state.hour} className='form__input' onChange={this.inputHandler} placeholder={'HH:MM'}/></label>
                 <button className='form__button'>Dodaj</button>
             </form>
         )
     }
 
     inputHandler = e => {
-        const {name,value} = e.target;
-        console.log(name,value);
+        const targetName = e.target.name;
+
+        if(targetName ==='firstName'){
+            const firstName = e.target.value
+            this.checkPersonDate(firstName,targetName)
+        }
+
+        if(targetName ==='lastName'){
+            const lastName = e.target.value;
+            this.checkPersonDate(lastName,targetName)
+        }
+
+        if(targetName ==='email'){
+            const email = e.target.value;
+            this.checkEmail(email,targetName)
+        }
+
+        if(targetName ==='date'){
+            const date = e.target.value;
+            this.checkDay(date,targetName)
+        }
+
+        if(targetName ==='hour'){
+            const hour = e.target.value;
+            this.checkHour(hour,targetName)
+        }
+    }
+
+    addToLocalState(value,targetName){
         this.setState({
-            [name]:value
-        })
+            [targetName]:value
+        });
+    }
+
+    checkPersonDate(value,targetName){
+        const regExp = /^[a-zA-Z]{2,30}/;
+        if(value.match(regExp)){
+            console.log('imie lub nazwisko poprawne');
+            this.addToLocalState(value,targetName)
+        }
+        return false
+    }
+
+    checkEmail(value,targetName){
+        const regExp = /^[-\w\.]+@([-\w]+\.)+[a-z]+$/i;
+        if(value.match(regExp)){
+            console.log('email poprawny');
+            this.addToLocalState(value,targetName)
+        }
+        console.log('email zly');
+        return false
+    }
+
+    checkDay(value,targetName){
+        const regExp = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
+        if(value.match(regExp)){
+            console.log('data poprawna');
+            this.addToLocalState(value,targetName)
+        }
+        console.log('data zla');
+        return false
+    }
+
+    checkHour(value,targetName){
+        const regExp = /^([0-1][0-9]|[2][0-3]):([0-5][0-9])$/;
+        if(value.match(regExp)){
+            console.log('godzina poprawna');
+            this.addToLocalState(value,targetName)
+        }
+        console.log('godzina zla');
+        return false
     }
 
     submitHandler = e =>{
